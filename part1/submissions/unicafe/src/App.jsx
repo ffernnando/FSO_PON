193ecbf4899a021a505a89b44de0dc96f16731e2
Fernando <ffernnando192@gmail.com>
2026-02-26 17:51:56 +0100

    Exercises 1.6.-1.11.

---
+++ part1/submissions/unicafe/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / (total);
+  const positive = good / (total) * 100;
+  if (total) {
+    return(
+      <div>
+        <h1>statistics</h1>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good}/>
+            <StatisticLine text="neutral" value={neutral}/>
+            <StatisticLine text="bad" value={bad}/>
+            <StatisticLine text="all" value={total}/>
+            <StatisticLine text="average" value={average}/>
+            <StatisticLine text="positive" value={`${positive}%`}/>
+          </tbody>
+        </table>
+      </div>
+    )
+  } else {
+    return (<p>No feedback given</p>);
+  }
+  
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {setGood(good + 1)}
+  const handleNeutralClick = () => {setNeutral(neutral + 1)}
+  const handleBadClick = () => {setBad(bad + 1)};
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" onClick={handleGoodClick} />
+      <Button text="neutral" onClick={handleNeutralClick} />
+      <Button text="bad" onClick={handleBadClick} />
+
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+      
+      
+    </div>
+  )
+}
+
+export default App
--- part1/wf/part1c/src/App.jsx
+++ part1/wf/part1c/src/App.jsx
@@ -1,45 +1,49 @@
-import { useState } from 'react'
+import { useState } from "react"
 
-const Display = ({counter}) => {
-
-  return(
-    <div>{counter}</div>
-  );
-}
-
-const Button = ({ onClick, text }) => <button onClick={onClick}> {text} </button>
-
-const App = () => {
-
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value: ', counter);
-
-
-  const increaseByOne = () => {
-    console.log('increasing, value brefore: ', counter);
-    setCounter(counter + 1)
-  };
-
-  const decreaseByOne = () => {
-    console.log('decreasing, value before: ', counter);
-    setCounter(counter - 1);
-  } 
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before: ', counter);
-    setCounter(0);
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
   }
-
-  console.log('rendering...', counter)
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-export default App
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+export default App;
