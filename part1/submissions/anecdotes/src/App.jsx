c7a7be0c1aa35df2583805baefcaa6301954c086
Fernando <ffernnando192@gmail.com>
2026-02-26 18:28:00 +0100

    Exercises 1.12.-1.14.

---
+++ part1/submissions/anecdotes/src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+
+const Anecdote = ({ text, votes }) => (
+  <div>
+    <p>{text}</p>
+    <p>has {votes} votes</p>
+  </div>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(8).fill(0));
+  const [topVoted, setTopVoted] = useState(0);
+  
+  useEffect(() => {
+    setSelected(Math.floor(Math.random() * 8));
+  }, []);
+
+  console.log('Selected quote: ' + selected);
+
+  const handleRandomClick = () => setSelected(Math.floor(Math.random() * (8 - 0) + 0));
+  const handleVoteClick = () => {
+    console.log('voting!')
+    const newVotes = [...votes]
+    newVotes[selected] += 1;
+    setTopVoted(votes.indexOf(Math.max(...votes)))
+    setVotes(newVotes);
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <Anecdote text={anecdotes[selected]} votes={votes[selected]} />
+      <button onClick={handleVoteClick}>vote</button>
+      <button onClick={handleRandomClick}>next anecdote</button>
+
+      <h1>Anecdote with most votes</h1>
+      <Anecdote text={anecdotes[topVoted]} votes={votes[topVoted]} />
+    </div>
+  )
+}
+
+export default App
